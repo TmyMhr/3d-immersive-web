bf3d920e3f51fa359f4e67e0271be25e6a830dfd
Tommy <tommymaher10@gmail.com>
2022-05-16 16:10:31 +0100

    removed clouds
    clouds won't work in production due to reliance on cdn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber"
-import { Sky, Stars,  PointerLockControls, Stats, Cloud  } from "@react-three/drei"
+import { Sky, Stars,  PointerLockControls, Stats } from "@react-three/drei"
 import { Physics } from "@react-three/cannon"
 import { Ground } from "./Ground"
 import { Player } from "./Player"
@@ -15,13 +15,7 @@ export default function App() {
       <Stats /> 
  
       <Sky sunPosition={[100, 20, 100]} />
-      <Cloud
-  opacity={0.5}
-  speed={0.4} // Rotation speed
-  width={10} // Width of the full cloud
-  depth={1.5} // Z-dir depth
-  segments={20} // Number of particles
-/>
+
       <Stars />
       <ambientLight intensity={0.5} />
       <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
