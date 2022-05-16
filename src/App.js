39f13a962728fe1841c36df17bf5f0c7438fa765
Tommy <tommymaher10@gmail.com>
2022-05-16 15:34:01 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Canvas } from "@react-three/fiber"
-import { Sky, Stars,  PointerLockControls, Stats } from "@react-three/drei"
+import { Sky, Stars,  PointerLockControls, Stats, Cloud  } from "@react-three/drei"
 import { Physics } from "@react-three/cannon"
 import { Ground } from "./Ground"
 import { Player } from "./Player"
@@ -11,9 +11,17 @@ import { UserInterFaceTest } from "./userInterfaceTest"
 export default function App() {
   return (
     <Canvas shadows gl={{ alpha: false }} camera={{ fov: 45 }}>
+
       <Stats />
+      <Cloud
+  opacity={0.5}
+  speed={0.4} // Rotation speed
+  width={10} // Width of the full cloud
+  depth={1.5} // Z-dir depth
+  segments={20} // Number of particles
+/> 
       <Sky sunPosition={[100, 20, 100]} />
-  <Stars />
+      <Stars />
       <ambientLight intensity={0.5} />
       <pointLight castShadow intensity={0.8} position={[100, 100, 100]} />
       <Physics gravity={[0, -30, 0]}>
