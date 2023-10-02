cbbc94c373418951aadd1d296f1ac6ac0cbf7f80
Tommy <tommymaher10@gmail.com>
2023-10-02 18:17:19 +0100

    fix

--- src/Ground.js
+++ src/Ground.js
@@ -1,24 +1,16 @@
-import * as THREE from "three";
-import { useLoader } from "@react-three/fiber";
-import { useTerrain } from "@react-three/drei";
-import heightmap from "./assets/heightmap-test.png"; // Replace with your heightmap image
+import * as THREE from "three"
+import { useLoader } from "@react-three/fiber"
+import { usePlane } from "@react-three/cannon"
+import glass from "./assets/glass.png"
 
 export const Ground = (props) => {
-  const [ref] = useTerrain(() => ({
-    width: 1000, // Adjust the width of your terrain
-    height: 1000, // Adjust the height of your terrain
-    widthSegments: 100, // Adjust the number of segments
-    heightSegments: 100, // Adjust the number of segments
-    ...props,
-  }));
-
-  const texture = useLoader(THREE.TextureLoader, heightmap);
-  texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
+  const texture = useLoader(THREE.TextureLoader, glass)
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   return (
     <mesh ref={ref} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[1000, 1000, 100, 100]} />
-      <meshStandardMaterial map={texture} map-repeat={[10, 10]} color="white" />
+      <planeGeometry args={[1000, 1000]} />
+      <meshStandardMaterial map={texture} map-repeat={[100000, 100000]} color="white" />
     </mesh>
-  );
-};
+  )
+}
