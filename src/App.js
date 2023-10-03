6b460e270583ca75cf85cfa2cbcb6254c7a129c1
Tommy <tommymaher10@gmail.com>
2023-10-03 01:42:56 +0100

    we got rocks!

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-
-import { Canvas } from "@react-three/fiber"
-import { Sky, Stars, Cloud, PointerLockControls, Stats } from "@react-three/drei"
-import { Physics } from "@react-three/cannon"
-import { Ground } from "./Ground"
-import { Player } from "./Player"
-
-import { Cubes } from "./Cube"
-import { UserInterFaceTest } from "./userInterfaceTest"
-// refactored and updated to remove use-cannon and drei. Much more utilities available now. still need to fix some small lighting issues but almost there. 
-
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Sky, Stars, PointerLockControls, Stats } from '@react-three/drei';
+import { Physics } from '@react-three/cannon';
+import { Ground } from './Ground';
+import { Player } from './Player';
+import Model from "./Model" // Import the Model component as a named import
+import { Cubes } from './Cube';
+import { UserInterFaceTest } from './userInterfaceTest';
 
 export default function App() {
   return (
     <Canvas shadows gl={{ alpha: false }} camera={{ fov: 45 }}>
-
-      <Stats /> 
-      <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25}  /> 
-
+      <Stats />
+      <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
       <Stars />
       <ambientLight intensity={0.5} />
       <pointLight castShadow intensity={0.5} position={[100, 100, 100]} />
       <Physics gravity={[0, -30, 0]}>
         <Ground />
         <Player />
-      <UserInterFaceTest />
-        <Cubes />  
+        <Model />
+        <UserInterFaceTest />
+        <Cubes />
+        {/* Add the ModelViewer component here */}
+
       </Physics>
       <PointerLockControls />
     </Canvas>
-  )
+  );
 }
---
+++ src/Model.js
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+import { useGLTF } from '@react-three/drei';
+
+export default function Model(props) {
+  const group = useRef();
+  const { nodes, materials, errors } = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/rocks-forrest/model.gltf'
+  );
+
+  if (errors) {
+    console.error('Error loading model:', errors);
+    return null; // Render nothing or a placeholder in case of an error
+  }
+
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh geometry={nodes.rocksA_forest.geometry} material={materials['Stone.007']} rotation={[Math.PI / 2, 0, 0]} />
+    </group>
+  );
+}
+
+useGLTF.preload('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/rocks-forrest/model.gltf');
