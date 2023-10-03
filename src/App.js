1d25004ac582e7eec57d9727d8f5e8d06b60a54e
Tommy <tommymaher10@gmail.com>
2023-10-03 02:21:44 +0100

    added randomness to the spawning and rotation of objects!

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Sky, Stars, PointerLockControls, Stats } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
-import Model from "./Model" // Import the Model component as a named import
+import Model from './Model'; // Import the Model component as a named import
 import { Player } from './Player';
 import { Ground } from './Ground';
 import { Cubes } from './Cube';
 import { UserInterFaceTest } from './userInterfaceTest';
-
-
-
 
 function generateRandomPosition(min, max, baseY) {
   const x = Math.random() * (max - min) + min;
@@ -17,18 +14,23 @@ function generateRandomPosition(min, max, baseY) {
   return [x, baseY, z];
 }
 
-
+function generateRandomRotationY() {
+  const randomRotationY = (Math.random() - 0.5) * Math.PI * 2; // Random rotation around Y axis
+  return [0, randomRotationY, 0]; // Only rotate around Y-axis
+}
 
 export default function App() {
-
   const numModels = 10; // Adjust the number of models you want to spawn
   const minPosition = -10; // Adjust the minimum position for randomization
   const maxPosition = 10; // Adjust the maximum position for randomization
   const baseY = 0; // Adjust the desired base Y coordinate
 
-
   const randomPositions = Array.from({ length: numModels }, () =>
     generateRandomPosition(minPosition, maxPosition, baseY)
+  );
+
+  const randomRotations = Array.from({ length: numModels }, () =>
+    generateRandomRotationY()
   );
 
   return (
@@ -41,13 +43,12 @@ export default function App() {
       <Physics gravity={[0, -30, 0]}>
         <Ground />
         <Player />
-        {randomPositions.map((position, index) => (
-        <Model key={index} position={position} />
-      ))}
+        {Array.from({ length: numModels }).map((_, index) => (
+          <Model key={index} position={randomPositions[index]} rotation={randomRotations[index]} />
+        ))}
         <UserInterFaceTest />
         <Cubes />
         {/* Add the ModelViewer component here */}
-
       </Physics>
       <PointerLockControls />
     </Canvas>
