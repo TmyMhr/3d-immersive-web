580582ea3345b2ea5bd11ac906ef103ca5b5ba98
Tommy <tommymaher10@gmail.com>
2023-10-03 09:16:05 +0100

    latest push: updated lighting and physics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import * as THREE from "three";
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Sky, Stars, PointerLockControls, Stats } from '@react-three/drei';
+import { Sky, PointerLockControls, Stats } from '@react-three/drei';
 import { Physics } from '@react-three/cannon';
 import Model from './Model'; // Import the Model component as a named import
 import { Player } from './Player';
@@ -36,10 +37,22 @@ export default function App() {
   return (
     <Canvas shadows gl={{ alpha: false }} camera={{ fov: 45 }}>
       <Stats />
-      <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <pointLight castShadow intensity={0.5} position={[100, 100, 100]} />
+      <Sky
+  distance={450000}
+  sunPosition={[0, 0.3, 0]} // Lower Y-coordinate for a setting sun
+  inclination={0.2}          // Lower inclination for a setting sun
+  azimuth={0.8}           // Adjust horizontal position
+/>
+ 
+<ambientLight intensity={0.5} color={new THREE.Color(0xffcc66)} /> {/* Warm yellow color */}
+      
+      {/* Point Light */}
+      <pointLight
+        castShadow
+        intensity={0.5}
+        position={[10, 10, 10]} // Adjust position as needed
+        color={new THREE.Color(0xffaa33)} // Warm orange color
+      />
       <Physics gravity={[0, -30, 0]}>
         <Ground />
         <Player />
